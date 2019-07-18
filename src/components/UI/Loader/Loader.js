2467cca00505fa523f250c9d71084e7bb3af013d
Kartik Grewal <kartik.grewal@gmail.com>
2019-07-18 11:07:10 +0530

    test(Loader): Added unit tests, props and documentation
    Added unit tests, props and documentation

--- src/components/UI/Loader/Loader.js
+++ src/components/UI/Loader/Loader.js
@@ -1,22 +1,36 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-import styles from './Loader.scss';
+import styles from "./Loader.scss";
 
-const Loader = ({ size, className }) => {
+const Loader = ({ size, className, prefixClassName }) => {
   return (
-    <div data-testid="loader" className={`${styles.loader} ${className}`}>
-      <span className={styles.loaderInner} />
+    <div
+      data-testid="loader"
+      className={`${styles.loader} ${prefixClassName} ${className}`}
+      style={{ width: size, height: size }}
+    >
+      <span
+        data-testid="inner-loader"
+        className={`${styles.loaderInner} ${prefixClassName}-inner`}
+      />
     </div>
   );
 };
 
 Loader.propTypes = {
+  /** Size of the loader to be shown */
   size: PropTypes.number,
+  /** The classname to be appended to the outermost element */
+  className: PropTypes.string,
+  /** prefix to be appended before classname to the child elements */
+  prefixClassName: PropTypes.string,
 };
 
 Loader.defaultProps = {
   size: 40,
+  className: "",
+  prefixClassName: "",
 };
 
 export default Loader;
--- src/components/UI/Loader/__tests__/Loader.test.js
+++ src/components/UI/Loader/__tests__/Loader.test.js
@@ -1,19 +1,34 @@
 /* eslint-env jest */
 
-import React from 'react';
-import 'jest-dom/extend-expect';
-import { render, fireEvent, cleanup } from '@testing-library/react';
+import React from "react";
+import "jest-dom/extend-expect";
+import { render, cleanup } from "@testing-library/react";
 
-import Loader from '../Loader';
+import Loader from "../Loader";
 
 afterEach(cleanup);
 
-test('Smoke', async () => {
+test("Smoke", async () => {
   const { queryAllByTestId } = render(<Loader />);
-  expect(queryAllByTestId('loader').length).toBe(1)
+  expect(queryAllByTestId("loader").length).toBe(1);
 });
 
-test('Custom class', async () => {
-  const { getByTestId } = render(<Loader className="test"/>);
-  expect(getByTestId('loader')).toHaveClass('test');
+test("Custom class", async () => {
+  const { getByTestId } = render(<Loader className="test" />);
+  expect(getByTestId("loader")).toHaveClass("test");
+});
+
+test("Prefix custom class", async () => {
+  const { getByTestId } = render(
+    <Loader className="test" prefixClassName="pre" />,
+  );
+  expect(getByTestId("loader")).toHaveClass("pre");
+  expect(getByTestId("loader")).toHaveClass("test");
+  expect(getByTestId("inner-loader")).toHaveClass("pre-inner");
+});
+
+test("Size variations", async () => {
+  const { getByTestId } = render(<Loader size={40} />);
+  expect(getByTestId("loader")).toHaveStyle("width: 40px");
+  expect(getByTestId("loader")).toHaveStyle("height: 40px");
 });
