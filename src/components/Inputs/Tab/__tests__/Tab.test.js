6c963b78be6f62d485b761be8fcf5d2282a1cd6c
Kartik Grewal <kartik.grewal@gmail.com>
2019-08-01 19:38:12 +0530

    refactor(Tab): Docs, examples and tests for Tab
    Docs, examples and tests for Tab

---
+++ src/components/Inputs/Tab/__tests__/Tab.test.js
@@ -0,0 +1,94 @@
+/* eslint-env jest */
+
+import React from "react";
+import "jest-dom/extend-expect";
+import { render, cleanup, fireEvent } from "@testing-library/react";
+
+import Tab from "../Tab";
+
+afterEach(cleanup);
+
+test("Smoke", async () => {
+  const { queryAllByTestId } = render(<Tab />);
+  expect(queryAllByTestId("tab").length).toBe(1);
+});
+
+test("Custom class", async () => {
+  const { getByTestId } = render(
+    <Tab className="test" prefixClassName="pre" />,
+  );
+  expect(getByTestId("tab")).toHaveClass("test");
+});
+
+test("prefixClassName test", async () => {
+  const classNames = Object.keys(Tab.classNames).map(className =>
+    className.replace("$prefix", "test"),
+  );
+
+  const { container } = render(
+    <Tab
+      label="Checking"
+      prefixClassName="test"
+      options={["first", "second"]}
+      value="first"
+    />,
+  );
+
+  classNames.forEach(className => {
+    expect(
+      document.getElementsByClassName(className).length,
+      className,
+    ).toBeGreaterThanOrEqual(1);
+  });
+});
+
+test("Testing on focus", async () => {
+  const mockFn = jest.fn();
+  const { getByTestId } = render(
+    <Tab
+      label="Checking"
+      className="test"
+      prefixClassName="pre"
+      options={["first", "second"]}
+      onFocus={mockFn}
+    />,
+  );
+  const button = document.getElementsByClassName("test")[0];
+
+  fireEvent.focus(button);
+  expect(mockFn).toHaveBeenCalled();
+});
+
+test("Testing on blur", async () => {
+  const mockFn = jest.fn();
+  const { getByTestId } = render(
+    <Tab
+      label="Checking"
+      className="test"
+      prefixClassName="pre"
+      options={["first", "second"]}
+      onBlur={mockFn}
+    />,
+  );
+  const button = document.getElementsByClassName("test")[0];
+
+  fireEvent.blur(button);
+  expect(mockFn).toHaveBeenCalled();
+});
+
+test("On change function", async () => {
+  const mockFn = jest.fn();
+
+  const { getByTestId } = render(
+    <Tab
+      onChange={mockFn}
+      options={["first", "second"]}
+      value="second"
+      prefixClassName="test"
+    />,
+  );
+
+  const tabs = document.getElementsByClassName("test-option")[0];
+  fireEvent.click(tabs);
+  expect(mockFn).toHaveBeenCalled();
+});
