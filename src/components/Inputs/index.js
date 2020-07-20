bc7ec687868a74284c0f391f2aab1101d23c3767
Siddhant Mulay <siddhant.mulay@haptik.ai>
2020-07-20 12:04:57 +0530

    fix(expandable-input-bar): update export for expandable input bar

--- src/components/Inputs/index.js
+++ src/components/Inputs/index.js
@@ -12,7 +12,7 @@ import Date from "./Date";
 import Time from "./Time";
 import RangeSlider from "./RangeSlider";
 import NumberCounter from "./NumberCounter";
-import ExpandableSearchBar from "./ExpandableSearchBar";
+import ExpandableInputBar from "./ExpandableInputBar";
 
 const Input = {
   Toggle,
@@ -29,7 +29,7 @@ const Input = {
   Time,
   RangeSlider,
   NumberCounter,
-  ExpandableSearchBar,
+  ExpandableInputBar,
 };
 
 export default Input;
