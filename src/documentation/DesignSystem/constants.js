b1d96dc2c5860f94271985e2ca74d641d419686c
Siddhant Mulay <siddhant.mulay@haptik.ai>
2021-03-08 13:54:03 +0530

    docs(design-system): add design system documentation

---
+++ src/documentation/DesignSystem/constants.js
@@ -0,0 +1,103 @@
+export const COLORS_DATA = {
+  columns: [
+    {
+      title: "Model",
+    },
+    {
+      title: "Token",
+    },
+    {
+      title: "Example",
+    },
+    {
+      title: "Color Variant",
+    },
+  ],
+  data: [
+    [
+      { content: "Primary" },
+      { content: "Haptik" },
+      { content: "Haptik Blue" },
+      { content: "blue.haptik" },
+    ],
+    [
+      { content: "Pastels" },
+      { content: "Pastel" },
+      { content: "Pastel Pink" },
+      { content: "pink.pastel" },
+    ],
+    [
+      { content: "Softs" },
+      { content: "Soft" },
+      { content: "Soft Grey" },
+      { content: "grey.soft" },
+    ],
+    [
+      { content: "Neons" },
+      { content: "Neon" },
+      { content: "Neon Green" },
+      { content: "green.neon" },
+    ],
+    [
+      { content: "Deep" },
+      { content: "Deep" },
+      { content: "Deep Red" },
+      { content: "red.deep" },
+    ],
+    [
+      { content: "Darks" },
+      { content: "Dark" },
+      { content: "Dark Teal" },
+      { content: "teal.dark" },
+    ],
+    [
+      { content: "Blacks" },
+      { content: "Black" },
+      { content: "Haptik Black" },
+      { content: "black.haptik" },
+    ],
+  ],
+};
+
+export const FONT_DATA = {
+  columns: [
+    {
+      title: "Token / Variant",
+    },
+    {
+      title: "Font Size Token",
+    },
+    {
+      title: "Font Size",
+    },
+    {
+      title: "Font Weight",
+    },
+  ],
+  data: [
+    [
+      { content: "Heading" },
+      { content: "lg" },
+      { content: "18px" },
+      { content: "bold" },
+    ],
+    [
+      { content: "Body" },
+      { content: "md" },
+      { content: "16px" },
+      { content: "normal" },
+    ],
+    [
+      { content: "Secondary" },
+      { content: "sm" },
+      { content: "14px" },
+      { content: "normal" },
+    ],
+    [
+      { content: "Tertiary" },
+      { content: "xs" },
+      { content: "12px" },
+      { content: "thin" },
+    ],
+  ],
+};
--- src/documentation/index.storybook.js
+++ src/documentation/index.storybook.js
@@ -6,6 +6,10 @@ import Installation from "./Installation.mdx";
 import Usage from "./Usage.mdx";
 import Theming from "./Theming.mdx";
 
+import DesignSystem from "./DesignSystem/DesignSystem.mdx";
+import Colors from "./DesignSystem/Colors.mdx";
+import Typography from "./DesignSystem/Typography.mdx";
+
 storiesOf("Documentation|Getting Started", module)
   .add("Introduction", () => <Introduction />)
   .add("Installation", () => <Installation />)
@@ -19,3 +23,28 @@ storiesOf("Documentation|Contributing", module)
   .add("Introduction", () => <Theming />)
   .add("Reporting Issues", () => <Theming />)
   .add("Writing Tests", () => <Theming />);
+
+storiesOf("Documentation|Design System (NEW!)", module)
+  .add("Introduction", () => <DesignSystem />)
+  .add("Colors", () => <Colors />, {
+    options: {
+      showPanel: true,
+      panelPosition: "right",
+    },
+    design: {
+      type: "figma",
+      url:
+        "https://www.figma.com/file/yDUBndKU3aNUr2jFYeO1jT/Haptik-Design-System?node-id=403%3A0",
+    },
+  })
+  .add("Typography", () => <Typography />, {
+    options: {
+      showPanel: true,
+      panelPosition: "right",
+    },
+    design: {
+      type: "figma",
+      url:
+        "https://www.figma.com/file/9Ar7pOkAjofRbfU7N60OsK/Design-System-2020-Typography-Set?node-id=500%3A0",
+    },
+  });
