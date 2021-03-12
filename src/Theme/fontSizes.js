138e6d2bf992413574e60713607767592016a6ff
Gopal Ojha <gopal.ojha.1989@gmail.com>
2021-03-12 13:57:29 +0530

    feat(text): add typography variant

--- src/Theme/fontSizes.js
+++ src/Theme/fontSizes.js
@@ -1,6 +1,1 @@
-export default {
-  xs: 12,
-  sm: 14,
-  md: 16,
-  lg: 18,
-};
+export default [10, 12, 14, 16, 18, 20, 24, 28];
--- src/Theme/text.js
+++ src/Theme/text.js
@@ -1,18 +1,90 @@
 export const textVariant = {
-  heading: {
-    fontSize: "lg",
-    fontWeight: "bold",
+  "display-1": {
+    "semi-bold": {
+      fontWeight: "semiBold",
+      fontSize: "7",
+    },
+    bold: {
+      fontWeight: "bold",
+      fontSize: "7",
+    },
+  },
+  "display-2": {
+    "semi-bold": {
+      fontWeight: "semiBold",
+      fontSize: "6",
+    },
+    bold: {
+      fontWeight: "bold",
+      fontSize: "6",
+    },
+  },
+  h1: {
+    "semi-bold": {
+      fontWeight: "semiBold",
+      fontSize: "5",
+    },
+    bold: {
+      fontWeight: "bold",
+      fontSize: "5",
+    },
+  },
+  h2: {
+    "semi-bold": {
+      fontWeight: "semiBold",
+      fontSize: "4",
+    },
+    bold: {
+      fontWeight: "bold",
+      fontSize: "4",
+    },
+  },
+  h3: {
+    "semi-bold": {
+      fontWeight: "semiBold",
+      fontSize: "3",
+    },
+    bold: {
+      fontWeight: "bold",
+      fontSize: "3",
+    },
+  },
+  h4: {
+    "semi-bold": {
+      fontWeight: "semiBold",
+      fontSize: "2",
+    },
+    bold: {
+      fontWeight: "bold",
+      fontSize: "2",
+    },
+  },
+  h5: {
+    "semi-bold": {
+      fontWeight: "semiBold",
+      fontSize: "1",
+    },
+    bold: {
+      fontWeight: "bold",
+      fontSize: "1",
+    },
   },
   body: {
-    fontSize: "md",
-    fontWeight: "normal",
-  },
-  secondary: {
-    fontSize: "sm",
-    fontWeight: "normal",
-  },
-  tertiary: {
-    fontSize: "xs",
-    fontWeight: "thin",
+    xs: {
+      fontWeight: "normal",
+      fontSize: "0",
+    },
+    sm: {
+      fontWeight: "normal",
+      fontSize: "1",
+    },
+    md: {
+      fontWeight: "normal",
+      fontSize: "2",
+    },
+    lg: {
+      fontWeight: "normal",
+      fontSize: "3",
+    },
   },
 };
