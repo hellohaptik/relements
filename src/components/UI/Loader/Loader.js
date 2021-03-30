fd2723cf530d18fec838f7a068acd8c9f30454fe
Swapnil Misal <swapnil.misal@haptik.ai>
2021-03-30 11:55:04 +0530

    improvement(loader): add styled component capability (#463)

--- src/components/UI/Loader/Loader.js
+++ src/components/UI/Loader/Loader.js
@@ -2,22 +2,39 @@ import React from "react";
 import PropTypes from "prop-types";
 
 import Context from "@src/components/Context";
+import { ThemedLoaderWrapper, ThemedSvg, ThemedCircle } from "./ThemedLoader";
+
 import styles from "./Loader.scss";
 
-const Loader = ({ size, className, prefixClassName }) => {
+const Loader = props => {
+  if (props.themed) {
+    const loaderProps = {
+      ...props,
+      "data-testid": "themedLoader",
+    };
+
+    return (
+      <ThemedLoaderWrapper {...loaderProps}>
+        <ThemedSvg data-testid="themedLoaderInner">
+          <ThemedCircle data-testid="themedLoaderCircle" />
+        </ThemedSvg>
+      </ThemedLoaderWrapper>
+    );
+  }
+
   const { primaryColor } = React.useContext(Context);
   return (
     <div
       data-testid="loader"
-      className={`${styles.loader} ${prefixClassName} ${className}`}
+      className={`${styles.loader} ${props.prefixClassName} ${props.className}`}
       style={{
-        width: size,
-        height: size,
+        width: props.size,
+        height: props.size,
       }}
     >
       <svg
         data-testid="loader-inner"
-        className={`${styles.loaderInner} ${prefixClassName}-inner`}
+        className={`${styles.loaderInner} ${props.prefixClassName}-inner`}
         viewBox="25 25 50 50"
       >
         <circle
@@ -42,12 +59,14 @@ Loader.propTypes = {
   className: PropTypes.string,
   /** prefix to be appended before classname to the child elements */
   prefixClassName: PropTypes.string,
+  themed: PropTypes.bool,
 };
 
 Loader.defaultProps = {
   size: 32,
   className: "",
   prefixClassName: "",
+  themed: false,
 };
 
 export default Loader;
